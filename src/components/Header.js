abd3906cc450a9857049e7ba2ee21eecae87443b
Anatoly Karpov <karpik-ne@yandex.ru>
2023-02-13 12:39:00 +0700

    ПР12: хедер войти

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
+import { Route, Switch, Link } from "react-router-dom";
+
 function Header() {
     return (
       
       <header className="header">
         <div className="header__logo" /> 
+        <Switch>
+          <Route path='/signin'>
+            <Link to='/signup' className="header__link">Регистрация</Link>
+          </Route>
+        </Switch>
+
       </header>
     )
 }
