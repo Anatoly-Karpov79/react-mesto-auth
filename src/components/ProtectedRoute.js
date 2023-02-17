eaa1fa9ee04c5c9e1ef783d91a7d0cad5867a1c8
Anatoly Karpov <karpik-ne@yandex.ru>
2023-02-17 21:43:19 +0700

    1

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -5,9 +5,9 @@ import { Redirect  } from "react-router-dom";
 
 // этот компонент принимает другой компонент в качестве пропса
 // он также может взять неограниченное число пропсов и передать их новому компоненту
-const ProtectedRoute = ({ element: Component, ...props  }) => {
+const ProtectedRoute = ({ сomponent: Component, ...props  }) => {
   return (
-    props.loggedIn ? <Component {...props} /> : <Redirect  to="/singup" />
+    props.loggedIn ? <Component {...props} /> : <Redirect  to="/sing_up" />
 )}
 
 export default ProtectedRoute; 
